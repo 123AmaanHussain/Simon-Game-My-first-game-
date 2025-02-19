3fb90c764bcdca9eac626863bfd0ea5e6bd0bfa0
123AmaanHussain <70699617+123AmaanHussain@users.noreply.github.com>
2025-02-19 22:58:00 +0530

    Update game.js

--- game.js
+++ game.js
@@ -57,10 +57,10 @@ function checkAnswer(currentLevel) {
 
 function nextSequence() {
     userClickedPattern = [];
-    //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+    // Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
     level++;
 
-  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  // Inside nextSequence(), update the h1 with this change in the value of level.
     $("#level-title").text("Level " + level);
     
     
@@ -68,7 +68,7 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
   
-    //1. Use jQuery to select the button with the same id as the randomChosenColour
+    // select the button with the same id as the randomChosenColour
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   
     // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
