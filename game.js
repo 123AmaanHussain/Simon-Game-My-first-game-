77a84deab8bf26355e209bdcf7b2a00556c23ff0
123AmaanHussain <70699617+123AmaanHussain@users.noreply.github.com>
2025-02-19 22:55:34 +0530

    Updated game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ var level = 0;
 $(document).keypress(function() {
     if (!started) {
   
-      //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+      // The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
       $("#level-title").text("Level " + level);
       nextSequence();
       started = true;
@@ -69,10 +69,8 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
   
     //1. Use jQuery to select the button with the same id as the randomChosenColour
-    //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   
-    //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
     // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     // audio.play();
     playSound(randomChosenColour);
